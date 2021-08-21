9197d1926a6334ac5576da7994ea9b35b8035adb
mlkstrs <klikinfo.gogomall@gmail.com>
2021-08-21 12:44:41 +0700

    Finishing app in ch 3

---
+++ src/main/kotlin/utils/CalculateAreaUtils.kt
@@ -0,0 +1,16 @@
+package utils
+
+object CalculateAreaUtils {
+    //calculateAreaRectangle
+    fun calculateAreaRectangle(length: Int, width: Int): Int {
+        return length * width
+    }
+
+   fun calculateAreaCircle(radius: Int): Double {
+        return 3.14 * radius * radius
+    }
+
+    fun calculateAreaTriangle(base: Int, height: Int): Double {
+        return 0.5 * base * height
+    }
+}
